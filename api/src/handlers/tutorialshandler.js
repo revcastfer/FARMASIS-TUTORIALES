4584363f9812a049e0ebfb223dcd583c1625f75a
fernando revilla <revcast@hotmail.com>
2023-07-14 18:16:51 -0500

    nivelacion

--- api/src/handlers/tutorialshandler.js
+++ api/src/handlers/tutorialshandler.js
@@ -1,43 +1,36 @@
-const {getTutorials, controllerPost}=require("../controllers/tutorialsControllers.js")
+const {getTutorials, controllerPost,getById}=require("../controllers/tutorialsControllers.js")
 const path = require('path');
 
 
 let tutorialsHandler=async(req,res)=>{
-try {
-let respuesta=await getTutorials();
-res.status(200).json(respuesta)
+const {id}=req.query;
 
+try {
+let respuesta=id?await getById(id):await getTutorials();
+res.status(200).json(respuesta)
 }
 catch (error){ res.status(501).send(error.message)}
-
 };
 
-let postTutorials=async(req,res)=>{
-  
+
+let postTutorials=async(req,res)=>{  
 let {nombre,descripcion,categoria}=req.body;
 const video = req.file.filename;
-
-if (categoria.length===1){categoria=Number(categoria)};
-
+ if (categoria.length===1){categoria=Number(categoria)};
 
 try{
 	let rpta=await controllerPost(nombre,descripcion,categoria,video);
-
-res.status(200).json(rpta)
+    res.status(200).json(rpta)
 }
 catch(error){ res.status(501).json(error.message)}
-
 };
 
 
 let getVideo=(req,res)=>{
-
 const {name}=req.params;
-
-
-
 res.sendfile(path.join(__dirname,`../videos/${name}`) )
-}
+};
+
 
 
 
